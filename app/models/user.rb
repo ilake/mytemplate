a4aa1d2d198a5059c3fc1ae906683bf9b0c3566c
ilake <lake.ilakela+github@gmail.com>
2011-07-13 16:57:06 +0800

    annotate user model and test

--- app/models/user.rb
+++ app/models/user.rb
@@ -37,3 +37,56 @@ class User < ActiveRecord::Base
     Notifier.password_reset_instructions(self).deliver
   end
 end
+
+# == Schema Information
+#
+# Table name: users
+#
+#  id                  :integer(4)      not null, primary key
+#  nickname            :string(255)     not null
+#  email               :string(255)     not null
+#  cellphone           :string(255)
+#  active              :boolean(1)      default(FALSE)
+#  cached_slug         :string(255)
+#  crypted_password    :string(255)     not null
+#  password_salt       :string(255)     not null
+#  persistence_token   :string(255)     not null
+#  single_access_token :string(255)     not null
+#  perishable_token    :string(255)     not null
+#  login_count         :integer(4)      default(0), not null
+#  failed_login_count  :integer(4)      default(0), not null
+#  last_request_at     :datetime
+#  current_login_at    :datetime
+#  last_login_at       :datetime
+#  current_login_ip    :string(255)
+#  last_login_ip       :string(255)
+#  created_at          :datetime
+#  updated_at          :datetime
+#
+
+# == Schema Information
+#
+# Table name: users
+#
+#  id                  :integer(4)      not null, primary key
+#  nickname            :string(255)     not null
+#  email               :string(255)     not null
+#  cellphone           :string(255)
+#  active              :boolean(1)      default(FALSE)
+#  cached_slug         :string(255)
+#  crypted_password    :string(255)     not null
+#  password_salt       :string(255)     not null
+#  persistence_token   :string(255)     not null
+#  single_access_token :string(255)     not null
+#  perishable_token    :string(255)     not null
+#  login_count         :integer(4)      default(0), not null
+#  failed_login_count  :integer(4)      default(0), not null
+#  last_request_at     :datetime
+#  current_login_at    :datetime
+#  last_login_at       :datetime
+#  current_login_ip    :string(255)
+#  last_login_ip       :string(255)
+#  created_at          :datetime
+#  updated_at          :datetime
+#
+
